ae7edb3a151dba8b6dfef1e5908c6140c936928c
Stefan <stefanjivalino@gmail.com>
2023-08-11 22:48:36 +0700

    Install pod on projects

---
+++ Pods/Target Support Files/Pods-Coupski/Pods-Coupski-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Coupski : NSObject
+@end
+@implementation PodsDummy_Pods_Coupski
+@end
---
+++ Pods/Target Support Files/Pods-CoupskiTests/Pods-CoupskiTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CoupskiTests : NSObject
+@end
+@implementation PodsDummy_Pods_CoupskiTests
+@end
